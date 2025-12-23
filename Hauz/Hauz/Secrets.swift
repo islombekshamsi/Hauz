c82100f211e70b409fd4e411cff5ef9cd5ba6454
Islombek Shamsiev <167647904+islombekshamsi@users.noreply.github.com>
2025-12-23 16:07:05 -0500

    Add your credentials
    Removed sensitive Supabase credentials from Secrets.swift.

--- Hauz/Hauz/Secrets.swift
+++ Hauz/Hauz/Secrets.swift
@@ -2,6 +2,6 @@ import Foundation
 import Supabase
 
 let supabase = SupabaseClient(
-  supabaseURL: URL(string: "https://bwrpauovmxablkbxschv.supabase.co")!,
-  supabaseKey: "sb_publishable_LGnigtrJ5KwtYE4Lq4BC-Q_HTVr1zkh"
+  supabaseURL: URL(string: "")!,
+  supabaseKey: ""
 )
