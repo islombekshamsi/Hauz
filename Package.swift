d275fb4ec5d2fda6df7f3e6065dbaa3401e517c6
Islombek Shamsiev <167647904+islombekshamsi@users.noreply.github.com>
2025-12-24 02:25:36 -0500

    Upgrade iOS platform version to v17
    Updated the iOS platform version to v17.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "shoely",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v17) // Updated to the latest iOS version available
     ],
     products: [
         .library(
