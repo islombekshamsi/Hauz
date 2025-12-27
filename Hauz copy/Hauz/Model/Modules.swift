eee334aa1308d095049a1b08f0c76abc619a1d51
Islombek Shamsiev <167647904+islombekshamsi@users.noreply.github.com>
2025-12-27 03:14:34 -0500

    Refactor Profile and UpdateProfileParams structures

--- Hauz copy/Hauz/Model/Modules.swift
+++ Hauz copy/Hauz/Model/Modules.swift
@@ -27,25 +27,57 @@ struct Sneaker: Codable, Identifiable {
 }
 
 struct Profile: Decodable {
-  let username: String?
-  let fullName: String?
-  let gender: String?
-
-  enum CodingKeys: String, CodingKey {
-    case username
-    case fullName = "full_name"
-    case gender
-  }
+    let id: UUID?
+    let username: String?
+    let fullName: String?
+    let gender: String?
+    let avatarURL: String?
+    let brands: [String]?
+    let phoneNumber: String?
+    let priceMin: Double?
+    let priceMax: Double?
+    let swipedRightIds: [UUID]?
+    let swipedLeftIds: [UUID]?
+    let updatedAt: Date?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case username
+        case fullName = "full_name"
+        case gender
+        case avatarURL = "avatar_url"
+        case brands
+        case phoneNumber = "phone_number"
+        case priceMin = "price_min"
+        case priceMax = "price_max"
+        case swipedRightIds = "swiped_right_ids"
+        case swipedLeftIds = "swiped_left_ids"
+        case updatedAt = "updated_at"
+    }
 }
 
 struct UpdateProfileParams: Encodable {
-  let username: String
-  let fullName: String
-  let gender: String
-
-  enum CodingKeys: String, CodingKey {
-    case username
-    case fullName = "full_name"
-    case gender
-  }
+    let username: String?
+    let fullName: String?
+    let gender: String?
+    let avatarURL: String?
+    let brands: [String]?
+    let phoneNumber: String?
+    let priceMin: Double?
+    let priceMax: Double?
+    let swipedRightIds: [UUID]?
+    let swipedLeftIds: [UUID]?
+    
+    enum CodingKeys: String, CodingKey {
+        case username
+        case fullName = "full_name"
+        case gender
+        case avatarURL = "avatar_url"
+        case brands
+        case phoneNumber = "phone_number"
+        case priceMin = "price_min"
+        case priceMax = "price_max"
+        case swipedRightIds = "swiped_right_ids"
+        case swipedLeftIds = "swiped_left_ids"
+    }
 }
