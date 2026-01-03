7eeec86db68b60b1f495fd1573d0fa85c910657f
Islombek Shamsiev <167647904+islombekshamsi@users.noreply.github.com>
2026-01-02 23:50:29 -0500

    Add files via upload

---
+++ Hauz copy/Hauz/Feed/ViewParts/ViewModel/SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  Hauz
+//
+//  Created by Islom Shamsiev on 2026/1/2.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
